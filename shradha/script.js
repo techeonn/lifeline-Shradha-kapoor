4c02772f0129e3c1fc4f3d0f4707c846d731e47d
techeonn <164405964+techeonn@users.noreply.github.com>
2024-04-04 20:43:20 +0530

    Add files via upload

---
+++ shradha/script.js
@@ -0,0 +1,8 @@
+const slider = document.querySelector('.slider');
+
+function activate(e) {
+  const items = document.querySelectorAll('.item');
+  e.target.matches('.next') && slider.append(items[0]);
+  e.target.matches('.prev') && slider.append(items[items.ngth -1]);
+}
+document.addEventListener('click',activate,false);
